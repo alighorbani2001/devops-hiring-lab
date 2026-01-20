af25bcf50bb69eb956a6c4fa7c0d53e9ec4aa503
alighorbani2001 <alighorbanii4242@gmail.com>
2026-01-21 01:34:58 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "DevOps Hiring Lab - FastAPI"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
